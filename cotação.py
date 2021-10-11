02f46d6e92c6b009e6c5dacd763ad03ea189cd7f
MathOli <matheus.oliveira2100@hotmail.com>
2021-10-10 22:35:09 -0300

    Separação de funções

--- cotação.py
+++ cotação.py
@@ -1,14 +1,34 @@
 import requests
 import json
+import time
 
-moedas = ["binancecoin","coinary-token","ripple","smooth-love-potion","dogecoin","contentos"]
-cotacoes = []
 
-for moeda in moedas:
-    cotacao = requests.get(f"https://api.coingecko.com/api/v3/simple/price?ids={moeda}&vs_currencies=brl")
-    cotacao = cotacao.json()
-    cotacao = {"Crypto": moeda, "BRL" : cotacao[moeda]['brl'] }
-    cotacoes.append(cotacao)
 
-for crypto in cotacoes:
-    print(f"Crypto {crypto['Crypto'].upper()} : R$ {crypto['BRL']:.2f}")
+
+def cotacao():
+    moedas = ["binancecoin", "coinary-token", "ripple", "smooth-love-potion", "dogecoin", "contentos"]
+
+
+    for i in range(3):
+        cotacoes = capturarCotacao(moedas)
+        listarCotacoes(cotacoes)
+        time.sleep(5)
+        print(' ')
+
+def capturarCotacao(moedas):
+    cotacoes = []
+    for moeda in moedas:
+        cotacao = requests.get(f"https://api.coingecko.com/api/v3/simple/price?ids={moeda}&vs_currencies=brl")
+        cotacao = cotacao.json()
+        cotacao = {"Crypto": moeda, "BRL": cotacao[moeda]['brl']}
+        cotacoes.append(cotacao)
+
+    return cotacoes
+def listarCotacoes(cotacoes):
+
+    for crypto in cotacoes:
+        print(f"Crypto {crypto['Crypto'].upper()} : R$ {crypto['BRL']:.2f}")
+
+
+if __name__ == "__main__":
+    cotacao()
